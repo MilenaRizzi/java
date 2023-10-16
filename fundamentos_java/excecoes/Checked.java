527be4816d8447fcb5224b194edaeaf2bd62a809
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2023-10-16 19:01:32 -0300

    uso do try/catch

--- fundamentos_java/excecoes/Checked.java
+++ fundamentos_java/excecoes/Checked.java
@@ -5,9 +5,13 @@ import pessoa.Pessoa;
 public class Checked {
     // é preferível evitar a ocorrência de NullPointerException por meio de programação defensiva, em vez de apenas capturá-la. Certifique-se de que os objetos sejam inicializados corretamente e de que você esteja tratando os casos em que um objeto pode ser null de forma apropriada.
 
-    //poderia capturar e tratar com try/catch
     public static void main(String[] args) {
-        Pessoa p = null;
-        p.getCpf();
+        try {
+            Pessoa p = null;
+            p.getCpf();
+        } catch (NullPointerException e) {
+            System.out.println("Erro  --> " + e);
+        }
+      
     }
 }
