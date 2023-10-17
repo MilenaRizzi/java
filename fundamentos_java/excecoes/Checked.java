1e53eed82b1730e40baecb6931c72dd0a19876d7
Milena Clara Rizzi <116590085+MilenaRizzi@users.noreply.github.com>
2023-10-17 11:13:19 -0300

    inserção de comentário para retirar o alerta que estava no programa

--- fundamentos_java/excecoes/Checked.java
+++ fundamentos_java/excecoes/Checked.java
@@ -1,13 +1,13 @@
 package excecoes;
 
-import pessoa.Pessoa;
+//import pessoa.Pessoa;
 
 public class Checked {
     // é preferível evitar a ocorrência de NullPointerException por meio de programação defensiva, em vez de apenas capturá-la. Certifique-se de que os objetos sejam inicializados corretamente e de que você esteja tratando os casos em que um objeto pode ser null de forma apropriada.
     public static void main(String[] args) {
         try {
-            Pessoa p = null;
-            p.getCpf();
+       //     Pessoa p = null;
+       //     p.getCpf();
         } catch (NullPointerException e) {
             System.out.println("Erro  --> " + e);
         }
